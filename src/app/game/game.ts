43c5f2006a1e39a021ee9c1b7ea08a8346ce09f5
littledumbduck <ale0014@alu.medac.es>
2025-12-18 23:10:32 +0100

    feat: add initial game component with basic HTML structure and form list initialization

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -8,14 +8,37 @@ import { Form } from '../core/models/form';
   styleUrl: './game.css',
 })
 export class Game {
+  formList: Form[] = [];
+  maxForm: number = 4;
+
   constructor() {
-
-    this.render();
+    for (let index = 0; index < this.maxForm; index++) {
+      this.createForm();
+    }
   }
 
-  render() {
-    this.makeForm();
+  createForm() {
+    const form = new Form();
+    this.formList.push(form);
   }
+
 
   renderForm() {
+    // Render form to game.html inside game-main-container with ngFor creating a new div for each form
+    const gameMainContainer = document.getElementById('game-main-container');
+    for (let index = 0; index < this.formList.length; index++) {
+      const form = this.formList[index];
+      const formDiv = document.createElement('div');
+      formDiv.classList.add('form-container');
+      formDiv.style.backgroundColor = form.backgroundColor;
+      formDiv.style.shape = form.shape;
+      formDiv.style.shapeColor = form.shapeColor;
+      formDiv.style.number = form.number;
+      formDiv.style.numberColor = form.numberColor;
+      formDiv.style.text = form.text;
+      formDiv.style.textColor = form.textColor;
+      gameMainContainer.appendChild(formDiv);
+    }
+
   }
+}
